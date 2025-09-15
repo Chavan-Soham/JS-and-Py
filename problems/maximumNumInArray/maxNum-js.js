b03726baed78f5505446e35b5ae02462dd5a3579
Chavan-Soham <jamesarcturus11@gmail.com>
2025-09-15 12:52:41 +0530

    Maximum in array/list problem

---
+++ problems/maximumNumInArray/maxNum-js.js
@@ -0,0 +1,7 @@
+// write a program to get the maximum number inside an array
+
+function maxNum(arr) {
+    return Math.max(...arr)
+}
+
+console.log(maxNum([22, 34, 11, 2, 5, 12, 36, 55, 10, 1 , 2, 33]))
---
+++ problems/maximumNumInArray/maxNum-py.py
@@ -0,0 +1,6 @@
+# write a program to get the maximum number inside a list
+
+def maxNum(listOfNums):
+    return max(listOfNums)
+
+print(maxNum([22, 34, 11, 2, 5, 12, 36, 55, 10, 1 , 2, 33]))
